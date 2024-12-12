bc74fc822cb4dd65701ee1df6a11b0ec623a3e24
Apihub-SiamTheFrog <froggyapihub@gmail.com>
2024-12-12 22:22:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-{
-  "name": "json-server",
-  "version": "1.0.0",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1"
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const data = require('./data.json'); 
+
+app.use((req, res, next) => {
+  const user = req.headers['user'];
+  const pass = req.headers['pass'];
+
+  if (user === 'admin' && pass === 'password123') {
+    next(); 
+  } else {
+    res.status(401).send('Unauthorized');
   }
-  }
+});
+
+app.get('/', (req, res) => {
+  res.json(data); 
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
